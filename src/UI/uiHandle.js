ab85b5df9d5e7747c1df9ef759451e21f7b330b0
Kishlesh <itssocialkks@gmail.com>
2024-06-26 23:18:13 +0530

    Replace ts file with js file for sidebar webview ui

---
+++ src/UI/uiHandle.js
@@ -0,0 +1,130 @@
+const correctFileSelectButton = document.querySelector(".slect-correct-file");
+const incorrectFileSelectButton = document.querySelector(".select-wrong-file");
+const generatorFileSelectButton = document.querySelector(".select-generator-file");
+const selectElement = document.getElementById('testCaseCount');
+const startButton = document.querySelector(".start");
+const stopButton = document.querySelector(".stop");
+const resetButton = document.querySelector(".reset");
+const startDiv = document.querySelector(".start-button-div");
+const startImg = document.querySelector(".start-button-img");
+
+
+// Correct file select button event listener
+correctFileSelectButton?.addEventListener("click", () => {
+    webVscode.postMessage({
+        command: 'correctFileSelect',
+    });
+});
+
+// Incorrect file select button event listener
+incorrectFileSelectButton?.addEventListener("click", () => {
+    webVscode.postMessage({
+        command: 'incorrectFileSelect',
+    });
+});
+
+// Generator file select button event listener
+generatorFileSelectButton?.addEventListener("click", () => {
+    webVscode.postMessage({
+        command: 'generatorFileSelect',
+    });
+});
+
+
+// event listener for option list
+selectElement?.addEventListener("change", (event) => {
+    webVscode.postMessage({
+        command: 'testCountChanged',
+        count: event.target?.value,
+    });
+});
+
+// event listener for start button
+startButton?.addEventListener("click", (event) => {
+    webVscode.postMessage({
+        command: 'findTestCases'
+    });
+    if (stopButton && resetButton && startDiv && startImg) {
+        startDiv.style.display = 'none';
+        startImg.style.display = 'inline';
+        stopButton.disabled = false;
+        startButton.disabled = true;
+        resetButton.disabled = true;
+    }
+});
+
+
+stopButton?.addEventListener("click", (event) => {
+    webVscode.postMessage({
+        command: 'stopExecution'
+    });
+    stopButton.disabled = true;
+});
+
+resetButton?.addEventListener("click", (event) => {
+    webVscode.postMessage({
+        command: 'resetState'
+    });
+});
+
+window.addEventListener('message', (event) => {
+
+    const message = event.data;
+
+    switch (message.command) {
+        case 'updateFileName':
+            let correctFileInput = document.querySelector(".correct-file-input");
+            if (correctFileInput) {
+                if (message.fileObject["correct"]) {
+                    correctFileInput.value = message.fileObject["correct"].split(message.PATH_SEPARATOR).pop();
+                }
+                else {
+                    correctFileInput.value = '';
+                }
+            }
+            let incorrectFileInput = document.querySelector(".incorrect-file-input");
+            if (incorrectFileInput) {
+                if (message.fileObject["incorrect"]) {
+                    incorrectFileInput.value = message.fileObject["incorrect"].split(message.PATH_SEPARATOR).pop();
+                } else {
+                    incorrectFileInput.value = '';
+                }
+            }
+            let generatorFileInput = document.querySelector(".generator-file-input");
+            if (generatorFileInput) {
+                if (message.fileObject["generator"]) {
+                    generatorFileInput.value = message.fileObject["generator"].split(message.PATH_SEPARATOR).pop();
+                } else {
+                    generatorFileInput.value = '';
+                }
+            }
+            return;
+        case 'updateOutputs':
+            let testCaseTextArea = document.querySelector(".test-case");
+            let correctOutputTextArea = document.querySelector(".correct-output");
+            let incorrectOutputTextArea = document.querySelector(".incorrect-output");
+            if (testCaseTextArea && correctOutputTextArea && incorrectOutputTextArea) {
+                testCaseTextArea.value = message.receivedOutput[0];
+                correctOutputTextArea.value = message.receivedOutput[1];
+                incorrectOutputTextArea.value = message.receivedOutput[2];
+            }
+            return;
+        case 'enableStart':
+            if (startButton && startDiv && startImg) {
+                startButton.disabled = false;
+                startDiv.style.display = 'block';
+                startImg.style.display = 'none';
+            }
+            return;
+        case 'enableReset':
+            if (resetButton) {
+                resetButton.disabled = false;
+            }
+            return;
+        case 'disableStop':
+            if (stopButton){
+                stopButton.disabled = true;
+            }
+            return;
+    }
+});
--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,25 @@ import * as vscode from 'vscode';
 import { SidebarProvider } from './SidebarProvider';
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.commands.registerCommand('cp-edge-case.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from CP Edge Case!');
-	});
+	// vscode.commands.registerCommand('cp-edge-case.chatCopilot', async () => {
+	// 	const messages = [
+	// 		vscode.LanguageModelChatMessage.User('You are a cat! Your job is to explain computer science concepts in the funny manner of a cat. Always start your response by stating what concept you are explaining. Always include code samples.'),
+	// 		vscode.LanguageModelChatMessage.User('Explain red black trees')
+	// 	];
+
+	// 	const model = await vscode.lm.selectChatModels({
+	// 		vendor: 'copilot',
+	// 	});
+	// 	console.log("hello");
+	// 	const response = await model[0].sendRequest(messages);
+
+	// 	let responseText = "";
+	// 	for await (const fragment of response.text) {
+	// 		responseText += fragment;
+	// 	}
+	// 	vscode.window.showWarningMessage(responseText);
+
+	// });
 	const sidebarProvider = new SidebarProvider(context.extensionUri);
 	context.subscriptions.push(
 		vscode.window.registerWebviewViewProvider(
